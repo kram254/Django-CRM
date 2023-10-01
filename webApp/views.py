b29878c0482a4810f0edf3222269fcb0cade2e7f
Emmanuel Ndaliro <markorlando45@gmail.com>
2023-10-01 09:34:13 +0300

    Logout and Register function

--- webApp/views.py
+++ webApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 # Create your views here.
 def home(request):
@@ -24,4 +25,26 @@ def home(request):
 
 
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request, "You have been logged out. Bye for now!!")
+    return redirect('home')
+
+def register_user(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            
+            # Authentication and logging in
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "Hurray! You have been successfully registered")
+            return redirect('home')
+     
+    else:
+        form = SignUpForm()    
+        return render (request, "register.html", {'form':form})
+ 
+    return render (request, "register.html", {'form':form})    
